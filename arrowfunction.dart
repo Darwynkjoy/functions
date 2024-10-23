40fa309c5da9302db5c4ddfc982f544257b9b995
darwynkjoy <darwynkjoy@gmail.com>
2024-10-23 13:37:54 +0530

    daily push

--- arrowfunction.dart
+++
@@ -1,9 +0,0 @@
-double calculateIntrest(double principle,double time,double rate)=>
-    principle*time*rate/100;
-void main(){
-double principle=10000;
-double time=1.5;
-double rate=9.5;
-double intrest=calculateIntrest(principle,time,rate);
-print(intrest);
-}
